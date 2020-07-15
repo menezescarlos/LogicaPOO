6182cc43d7ec4196ecb8dfe9b4cae82f43d1716a
menezescarlos <menezesoliveira40@hotmail.com>
2020-07-14 21:45:02 -0300

    Primeiro exercicio LPOO
    Signed-off-by: menezescarlos <menezesoliveira40@hotmail.com>

---
+++ LogicaPOO/src/br/com/primeiroprojeto/MeuPrograma.java
@@ -0,0 +1,8 @@
+package br.com.primeiroprojeto;
+
+public class MeuPrograma {
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro projeto");
+		System.out.println("Meu segndo projeto");
+	}
+}
