9f3276d7cc2c31beac3ab9a1f892f3550876a66f
menezescarlos <menezesoliveira40@hotmail.com>
2020-07-14 22:05:14 -0300

    Atualização 1.0

---
+++ Logica POO/src/br/com/primeiroprojeto/MeuPrograma.java
@@ -0,0 +1,11 @@
+package br.com.primeiroprojeto;
+
+public class MeuPrograma {
+
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro projeto");
+		System.out.println("Meu segndo projeto");
+
+	}
+
+}
